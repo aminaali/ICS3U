ec211b10809e9640715f378d63dbe240b09f5fe9
323009274 <323009274@C-3ZWDYR1.tdsb.on.ca>
2016-03-02 14:44:44 -0500

    I have created a HelloWorld File

---
+++ ICS3U/src/ali/HelloWorld.java
@@ -0,0 +1,16 @@
+package ali;
+/* HelloWorld.Java
+ * This program prints Hello World to the console
+ * Amina Ali
+ * March 2 2016
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	// this prints hello world to the console
+	System.out.println("Hello World");	
+
+	}
+
+}
