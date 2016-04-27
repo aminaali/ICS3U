4c006167e6730df1f7d27a7b607d3f912e162922
Amina Ali <itsaminaali@hotmail.com>
2016-04-27 15:04:00 -0400

    happy & u know it

---
+++ ICS3U/src/ali/HappyAndYouKnowItSong.java
@@ -0,0 +1,26 @@
+package ali;
+/*
+ * Amina Ali
+ * This program plays the song happy & you know it
+ * 27/04/2016
+ */
+
+public class HappyAndYouKnowItSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		String [] verses = new String [] { "run around", "watch some tv", "eat a cake", "jump around", "stomp around", "go and sleep", "go and eat", "go play games", "go and run", "turn around"};
+		for (int x=0; x<=9; x++)
+		{
+		System.out.println("if your happy and you know it " + verses[x]);
+		System.out.println("if your happy and you know it " + verses[x]);
+		System.out.println("if your happy and you know it and you really want to show it " + verses[x]);
+		System.out.println("if your happy and you know it " + verses[x]);
+		System.out.println("");
+		}
+		}
+	}
+
+
